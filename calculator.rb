9cb2ce209bbb73b2af5fe0f5a6c5b7c267630b33
Scott Weldon <scott@scott-weldon.com>
2017-07-06 16:24:00 -0700

    Implement basic RPN functionality

---
+++ calculator.rb
@@ -0,0 +1,38 @@
+# A Reverse Polish Notation (RPN) calculator for On-Site assignment.
+# Copyright (C) 2017 Scott Weldon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Calculator
+  def initialize
+    @stack = []
+  end
+
+  def push(arg)
+    if arg.is_a? Numeric
+      @stack.push(arg)
+
+      return arg
+    elsif ['+', '-', '*', '/'].include?(arg)
+      right = @stack.pop()
+      left = @stack.pop()
+
+      # See https://stackoverflow.com/a/20019672/2747593
+      return left.method(arg).(right)
+    else
+      message = "argument must be number or operator. Argument was: " + arg
+      throw ArgumentError.new(message)
+    end
+  end
+end
